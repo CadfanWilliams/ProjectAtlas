5402a510c0ac558a6b9909dd9066f53608bed485
John Williams <cadfanw@gmail.com>
2025-04-01 15:49:13 +0100

    initial commit

---
+++ src/test/java/ProjectAtlas/ProjectAtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectAtlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
