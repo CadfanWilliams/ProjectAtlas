674232faa4c2b523267d42a18367d77e66c222d9
John Williams <cadfanw@gmail.com>
2025-04-03 14:23:08 +0100

    removing test that fails to test ci pipeline

--- src/test/java/ProjectAtlas/ProjectAtlasApplicationTests.java
+++ src/test/java/ProjectAtlas/ProjectAtlasApplicationTests.java
@@ -3,18 +3,11 @@ package ProjectAtlas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class ProjectAtlasApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void purposefully_fail() {
-		assertEquals("test", "testtest");
 	}
 
 }
