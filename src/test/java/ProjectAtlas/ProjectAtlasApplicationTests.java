2c9b4d66c9859a21ab90d4a114b85f80471d544e
John Williams <cadfanw@gmail.com>
2025-04-03 14:21:56 +0100

    creating test that fails to test ci pipeline

--- src/test/java/ProjectAtlas/ProjectAtlasApplicationTests.java
+++ src/test/java/ProjectAtlas/ProjectAtlasApplicationTests.java
@@ -3,11 +3,18 @@ package ProjectAtlas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ProjectAtlasApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void purposefully_fail() {
+		assertEquals("test", "testtest");
 	}
 
 }
