0ac1f2d264513397d4df4d5766f066e74130a1cf
John Williams <cadfanw@gmail.com>
2025-07-07 15:28:02 +0100

    removed error by 1 letter mistyped

--- src/test/java/ProjectAtlas/ForecastFetcherTest.java
+++ src/test/java/ProjectAtlas/ForecastFetcherTest.java
@@ -12,7 +12,7 @@ import java.net.http.HttpResponse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;L
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ForecastFetcherTest {
